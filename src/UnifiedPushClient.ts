ee81801a7df1f38192a28f9aa84fffe4e8c91af9
Massimiliano Ziccardi <mziccard@redhat.com>
2020-03-27 11:26:33 +0100

    Added base class to automate login

--- src/UnifiedPushClient.ts
+++ src/UnifiedPushClient.ts
@@ -2,17 +2,19 @@ import { applyPushApplicationFilter, PushApplication, PushApplicationFilter } fr
 import axios, { AxiosInstance } from 'axios';
 import { Variant, VariantFilter, applyVariantFilter } from './Variant';
 
-const REALM = 'aerogear';
-const CLIENT_ID = 'unified-push-server-js';
+const DEFAULT_REALM = 'aerogear';
+const DEFAULT_CLIENT_ID = 'unified-push-server-js';
 
 interface Credentials {
   kcUrl: string;
   username?: string;
   password?: string;
+  realm?: string;
+  client_id?: string;
   token?: string;
 }
 
-export class UnifiedPushClient {
+abstract class AbstractUnifiedPushClient {
   private readonly apiURL: string;
   private api: AxiosInstance;
   private readonly credentials?: Credentials;
@@ -27,10 +29,13 @@ export class UnifiedPushClient {
     if (this.credentials) {
       if (!this.credentials.token) {
         this.credentials.token = (
-          await axios.post(
-            `${this.credentials.kcUrl}/auth/realms/${REALM}/protocol/openid-connect/token`,
-            `grant_type=password&client_id=${CLIENT_ID}&username=${this.credentials.username}&password=${this.credentials.password}`
-          )
+            await axios.post(
+                `${this.credentials.kcUrl}/auth/realms/${this.credentials.realm ||
+                DEFAULT_REALM}/protocol/openid-connect/token`,
+                `grant_type=password&client_id=${this.credentials.client_id || DEFAULT_CLIENT_ID}&username=${
+                    this.credentials.username
+                }&password=${this.credentials.password}`
+            )
         ).data.access_token;
       }
       this.api = axios.create({ baseURL: this.apiURL, headers: { Authorization: `Bearer ${this.credentials.token}` } });
@@ -44,24 +49,15 @@ export class UnifiedPushClient {
      */
     find: async (filter?: PushApplicationFilter): Promise<PushApplication[]> => {
       await this.login();
-      let url = `${this.apiURL}/applications`;
-      if (filter && filter.id) {
-        url = `${url}/${filter.id}`;
-        return (await axios.get(url)).data;
-      } else {
-        let apps: PushApplication[] = (await axios.get(url)).data;
-        if (filter) {
-          apps = applyPushApplicationFilter(apps, filter);
-        }
-        return apps;
-      }
+      return this.findApplications(this.api, filter);
     },
     /**
      * Creates an application in the UPS
      * @param app the application to be created
      */
     create: async (app: PushApplication): Promise<PushApplication> => {
-      return (await axios.post(`${this.apiURL}/applications`, app)).data;
+      await this.login();
+      return this.createApplication(this.api, app);
     },
   };
 
@@ -73,29 +69,68 @@ export class UnifiedPushClient {
      */
     find: async (appId: string, filter?: VariantFilter): Promise<Variant[]> => {
       await this.login();
-      let variants: Variant[];
-      const url = `${this.apiURL}/applications/${appId}${filter?.type ? '/' + filter.type : ''}`;
-      const res = (await this.api.get(url)).data;
-      if (filter?.type) {
-        // ensure that returned object is an array
-        variants = res.filter ? res : [res];
-      } else {
-        // get all variants
-        const _variants = res.variants;
-        // ensure that returned object is an array
-        variants = _variants.filter ? _variants : [_variants];
-      }
-
-      return applyVariantFilter(variants, filter);
+      return this.findVariants(this.api, appId, filter);
     },
-
     /**
      * Creates a variant into the specified application
      * @param appId the id of the application
      * @param variant the variant to be created
      */
     create: async (appId: string, variant: Variant): Promise<Variant> => {
-      return (await axios.post(`${this.apiURL}/applications/${appId}/${variant.type}`, variant)).data as Variant;
+      await this.login();
+      return this.createVariant(this.api, appId, variant);
     },
   };
+
+  protected abstract async findApplications(api: AxiosInstance, filter?: PushApplicationFilter): Promise<PushApplication[]>;
+  protected abstract async createApplication(api: AxiosInstance, app: PushApplication): Promise<PushApplication>;
+
+  protected abstract async findVariants(api: AxiosInstance, appId: string, filter?: VariantFilter): Promise<Variant[]>;
+  protected abstract async createVariant(api: AxiosInstance, appId: string, variant: Variant): Promise<Variant>;
+}
+
+export class UnifiedPushClient extends AbstractUnifiedPushClient {
+
+  constructor(serverURL: string, credentials?: Credentials) {
+    super(serverURL, credentials);
+  }
+
+  protected async findApplications(api: AxiosInstance, filter?: PushApplicationFilter): Promise<PushApplication[]> {
+    let url = `/applications`;
+    if (filter && filter.id) {
+      url = `${url}/${filter.id}`;
+      return (await api.get(url)).data;
+    } else {
+      let apps: PushApplication[] = (await api.get(url)).data;
+      if (filter) {
+        apps = applyPushApplicationFilter(apps, filter);
+      }
+      return apps;
+    }
+  }
+
+  protected async createApplication(api: AxiosInstance, app: PushApplication): Promise<PushApplication> {
+    return (await api.post(`/applications`, app)).data;
+  }
+
+  protected async findVariants(api: AxiosInstance, appId: string, filter?: VariantFilter): Promise<Variant[]> {
+    let variants: Variant[];
+    const url = `/applications/${appId}${filter?.type ? '/' + filter.type : ''}`;
+    const res = (await api.get(url)).data;
+    if (filter?.type) {
+      // ensure that returned object is an array
+      variants = res.filter ? res : [res];
+    } else {
+      // get all variants
+      const _variants = res.variants;
+      // ensure that returned object is an array
+      variants = _variants.filter ? _variants : [_variants];
+    }
+
+    return applyVariantFilter(variants, filter);
+  }
+
+  protected async createVariant(api: AxiosInstance, appId: string, variant: Variant): Promise<Variant> {
+    return (await api.post(`/applications/${appId}/${variant.type}`, variant)).data as Variant;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,12 @@ async function run() {
   // console.log('apps', apps);
 
   //B868CC08-BCC8-4A0A-B21E-1AC56AF0C734
+  // const variants = await new UnifiedPushClient('http://localhost:9999', {
+  //   kcUrl: 'http://172.18.0.2:8080',
+  //   username: 'admin',
+  //   password: '123',
+  // }).variants.find('58939fc1-8868-46f5-95e3-0637ea97f9f4');
+
   const variants = await new UnifiedPushClient('http://localhost:9999', {
     kcUrl: 'http://172.18.0.2:8080',
     username: 'admin',
