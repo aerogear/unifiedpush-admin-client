f24cab1db83e6859022afaaffff1d3c29938b997
Andrew Brennan <andrew-brennan@live.com>
2020-04-07 18:09:24 +0100

    Fixes a merge conflict complaint

--- test/UnifiedPushAdminClient.test.ts
+++ test/UnifiedPushAdminClient.test.ts
@@ -1,88 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios';
-import { mockData } from './mockData';
-import { PushApplication, UnifiedPushAdminClient } from '../src';
-import { mocked } from 'ts-jest/dist/util/testing';
-import { VariantsAdmin } from '../src/variants/VariantsAdmin';
-
-jest.mock('axios');
-
-const mockedAxios = axios as jest.Mocked<typeof axios>;
-
-mocked(axios).create = () => mockedAxios;
-
-const upsClient = new UnifiedPushAdminClient('http://localhost:9999');
-
-describe('Applications Admin', () => {
-  it('test find without filters', async () => {
-    mockedAxios.get.mockImplementationOnce(() => Promise.resolve({ data: mockData }));
-
-    const res = await new UnifiedPushAdminClient('http://localhost:9999').applications.find();
-    expect(res).toHaveLength(mockData.length);
-    expect(mockedAxios.get).toHaveBeenCalledWith('/applications');
-  });
-  it('test filter by id - returning singe result', async () => {
-    mockedAxios.get.mockImplementationOnce(() => Promise.resolve({ data: mockData.find(value => value.id === '2') }));
-
-    const res = await new UnifiedPushAdminClient('http://localhost:9999').applications.find({
-      id: '2',
-    });
-    expect(res).toHaveLength(1);
-    expect(mockedAxios.get).toHaveBeenCalledWith('/applications/2');
-  });
-
-  it('test filter by id - returning array with one object', async () => {
-    mockedAxios.get.mockImplementationOnce(() => Promise.resolve({ data: [mockData.find(value => value.id === '2')] }));
-
-    const res = await new UnifiedPushAdminClient('http://localhost:9999').applications.find({
-      id: '2',
-    });
-    expect(res).toHaveLength(1);
-    expect(mockedAxios.get).toHaveBeenCalledWith('/applications/2');
-  });
-
-  it('test find filter by Developer', async () => {
-    mockedAxios.get.mockImplementationOnce(() => Promise.resolve({ data: mockData }));
-
-    const res = await new UnifiedPushAdminClient('http://localhost:9999').applications.find({
-      developer: 'Test Developer 2',
-    });
-    expect(res).toHaveLength(2);
-    expect(mockedAxios.get).toHaveBeenCalledWith('/applications');
-  });
-
-  it('test find filter by name', async () => {
-    mockedAxios.get.mockImplementationOnce(() => Promise.resolve({ data: mockData }));
-
-    const res = await new UnifiedPushAdminClient('http://localhost:9999').applications.find({
-      name: 'Application 3',
-    });
-    expect(res).toHaveLength(1);
-    expect(res[0]).toEqual(mockData.find(app => app.name === 'Application 3'));
-    expect(mockedAxios.get).toHaveBeenCalledWith('/applications');
-  });
-
-  it('test find with multiple filter', async () => {
-    mockedAxios.get.mockImplementationOnce(() => Promise.resolve({ data: mockData }));
-
-    const res = await new UnifiedPushAdminClient('http://localhost:9999').applications.find({
-      developer: 'Test Developer 2',
-      name: 'Application 4',
-    });
-    expect(res).toHaveLength(1);
-    expect(res[0].name).toEqual('Application 4');
-    expect(mockedAxios.get).toHaveBeenCalledWith('/applications');
-  });
-
-  it('test create app', async () => {
-    const appToBeCreated = mockData[3];
-    mockedAxios.post.mockImplementationOnce((url: string, app: PushApplication) => Promise.resolve({ data: app }));
-
-    const res = await new UnifiedPushAdminClient('http://localhost:9999').applications.create(appToBeCreated.name);
-    expect(res).toEqual({ name: appToBeCreated.name });
-    expect(mockedAxios.post).toHaveBeenCalledWith('/applications', { name: appToBeCreated.name });
-  });
-=======
 import * as nock from 'nock';
 import {
   BASE_URL,
@@ -102,7 +17,6 @@ import { KeycloakCredentials } from '../src/UnifiedPushAdminClient';
 beforeAll(() => {
   mockUps(BASE_URL, true);
   mockKeyCloak();
->>>>>>> bc6556cd51d0c7d51347baadc7256ea4cfbbb345
 });
 
 afterAll(() => {
@@ -139,18 +53,5 @@ describe('UnifiedPushAdminClient', () => {
       TEST_NEW_VARIANT_TO_CREATE
     );
     expect(variant).toEqual(TEST_NEW_VARIANT_CREATED);
-  });
-  //test deletion
-  it('test delete variants', async () => {
-    const appId = '1';
-    const selectedApp = mockData.filter(app => app.id === appId)[0];
-    mockedAxios.delete.mockImplementation(() =>
-      Promise.resolve({ data: selectedApp.variants!.filter(variant => variant.variantID === 'v-1:2') })
-    );
-    const expected = { variantID: 'v-1:2' };
-    const del = await upsClient.variants.delete(appId, { variantID: 'v-1:2' });
-    expect({ data: selectedApp.variants }).toEqual(expect.not.objectContaining(expected));
-    console.log({ data: selectedApp.variants });
-    console.log(del);
   });
 });
