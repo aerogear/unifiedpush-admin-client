02a22e9a4440f49c440ba15ffa51bac664c60adc
Massimiliano Ziccardi <mziccard@redhat.com>
2020-04-15 18:36:46 +0200

    Fixed header parsing. Bumped version

--- src/applications/ApplicationsAdmin.ts
+++ src/applications/ApplicationsAdmin.ts
@@ -1,43 +1,56 @@
-import { AxiosInstance } from 'axios';
+import { AxiosInstance, AxiosResponse } from 'axios';
 import { applyPushApplicationFilter, PushApplication, PushApplicationSearchOptions } from './PushApplication';
 
 export class ApplicationsAdmin {
+  // tslint:disable-next-line:no-any
+  ensureIsArray(obj: any) {
+    if (obj instanceof Array) {
+      return obj;
+    }
+    return [obj];
+  }
+
   async find(api: AxiosInstance, filter?: PushApplicationSearchOptions): Promise<PushApplication[]> {
     let url = `/applications`;
+    let response: AxiosResponse;
+
     if (filter && filter.pushApplicationID) {
       url = `${url}/${filter.pushApplicationID}`;
 
-      const response = await api.get(url, {
+      response = await api.get(url, {
         params: {
           includeDeviceCount: filter?.includeDeviceCount === true,
           includeActivity: filter?.includeActivity === true,
         },
       });
-
-      let res: PushApplication[] = response.data instanceof Array ? response.data : [response.data];
-
-      // Add deviceCount info and activityInfo to the PushApplication object
-      if (filter?.includeDeviceCount || filter?.includeActivity) {
-        // Add info to the app
-        res = res.map(app => {
-          app.activity = response.headers[`activity_app_${app.pushApplicationID}`];
-          app.deviceCount = response.headers[`deviceCount_app_${app.pushApplicationID}`];
-
-          // Add info to the variant
-          app.variants = app.variants?.map(variant => {
-            variant.activity = response.headers[`activity_variant_${app.pushApplicationID}`];
-            variant.deviceCount = response.headers[`deviceCount_variant_${app.pushApplicationID}`];
-            return variant;
-          });
-
-          return app;
-        });
-      }
-
-      return res;
     } else {
-      return applyPushApplicationFilter((await api.get(url)).data, filter);
+      response = await api.get(url, {
+        params: {
+          includeDeviceCount: filter?.includeDeviceCount === true,
+          includeActivity: filter?.includeActivity === true,
+        },
+      });
     }
+    let apps: PushApplication[] = applyPushApplicationFilter(this.ensureIsArray(response.data), filter);
+
+    // Add deviceCount info and activityInfo to the PushApplication object
+    if (filter?.includeDeviceCount || filter?.includeActivity) {
+      // Add info to the app
+      apps = apps.map((app: PushApplication) => {
+        app.activity = response.headers[`activity_app_${app.pushApplicationID?.toLowerCase()}`];
+        app.deviceCount = response.headers[`devicecount_app_${app.pushApplicationID?.toLowerCase()}`];
+
+        // Add info to the variant
+        app.variants = app.variants?.map(variant => {
+          variant.activity = response.headers[`activity_variant_${app.pushApplicationID?.toLowerCase()}`];
+          variant.deviceCount = response.headers[`devicecount_variant_${app.pushApplicationID?.toLowerCase()}`];
+          return variant;
+        });
+
+        return app;
+      });
+    }
+    return apps;
   }
 
   async create(api: AxiosInstance, name: string): Promise<PushApplication> {
